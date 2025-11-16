6b0d4a40e78703b8f08ca040335619c9a37fdd2e
calka2000 <simirian417@gmail.com>
2025-11-16 14:22:24 +0900

    ストレージ追加

--- rist.js
+++ rist.js
@@ -4,17 +4,35 @@ const attributeSelect = document.getElementById("attribute-select");
 const addBtn = document.getElementById("addbtn");
 const list = document.getElementById("todo-list");
 
+let tasks = [];
+window.addEventListener("load", () => {
+    const data = localStorage.getItem("tasks");
+    if (data) {
+        tasks = JSON.parse(data);
+        tasks.forEach(task => addTaskToList(task));
+    }
+});
+
 addBtn.addEventListener("click", () => {
     const task = {
+        id: Date.now(),
         text: input.value.trim(),
         difficulty: difficultySelect.value,
         attribute: attributeSelect.value
     };
     if (task.text === "") return;
+    tasks.push(task);
+    saveTasks();
     addTaskToList(task);
+
     input.value = "";
     difficultySelect.value = "易";
     attributeSelect.value = "生活";
+});
+
+    function saveTasks() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 
     function addTaskToList(task) {
         const li = document.createElement("li");
@@ -23,10 +41,12 @@ addBtn.addEventListener("click", () => {
         const delbtn = document.createElement("button");
         delbtn.textContent = "削除";
         delbtn.addEventListener("click", () => {
-            li.remove();
+            list.removeChild(li);
+            tasks = tasks.filter(t => t.id !== task.id);
+            saveTasks();
         });
 
         li.appendChild(delbtn);
         list.appendChild(li);
     }
-});
+
