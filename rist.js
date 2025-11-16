7ab67a4f1ebd998d19026194f58bc8fd65ec6a54
calka2000 <simirian417@gmail.com>
2025-11-16 13:27:08 +0900

    属性追加

--- rist.js
+++ rist.js
@@ -1,13 +1,18 @@
 const input = document.getElementById("todo-input");
+const difficultySelect = document.getElementById("difficulty-select");
+const attributeSelect = document.getElementById("attribute-select");
 const addBtn = document.getElementById("addbtn");
 const list = document.getElementById("todo-list");
 
 addBtn.addEventListener("click", () => {
     const text = input.value.trim();
+        const difficulty = difficultySelect.value;
+        const attribute = attributeSelect.value;
     if (text === "") return;
 
     const li = document.createElement("li");
-    li.textContent = text;
+
+    li.textContent = `${text}（難易度：${difficulty}、属性：${attribute}）`;
 
     const delbtn = document.createElement("button");
     delbtn.textContent = "削除";
@@ -18,4 +23,6 @@ addBtn.addEventListener("click", () => {
     li.appendChild(delbtn);
     list.appendChild(li);
     input.value = "";
+    difficultySelect.value = "易";
+    attributeSelect.value = "生活";
 });
