7708ca9e4d6748dda80b304abfcfa60fd2a5a0e7
calka2000 <simirian417@gmail.com>
2025-11-16 22:12:41 +0900

    ファイル消えてそうで嫌なので追加

--- rist.js
+++ rist.js
@@ -3,8 +3,14 @@ const difficultySelect = document.getElementById("difficulty-select");
 const attributeSelect = document.getElementById("attribute-select");
 const addBtn = document.getElementById("addbtn");
 const list = document.getElementById("todo-list");
+const charImg = document.getElementById("character");
+const pointView = document.getElementById("point-view");
+const resetBtn = document.getElementById("reset-btn");
 
 let tasks = [];
+let points = Number(localStorage.getItem("points")) || 0;
+updateCharacter();
+
 window.addEventListener("load", () => {
     const data = localStorage.getItem("tasks");
     if (data) {
@@ -32,11 +38,23 @@ addBtn.addEventListener("click", () => {
 
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("points", points);
 }
 
 function addTaskToList(task) {
     const li = document.createElement("li");
     li.textContent = `${task.text}（難易度：${task.difficulty}、属性：${task.attribute}）`;
+
+
+    const donebtn = document.createElement("button");
+    donebtn.textContent = "完了";
+    donebtn.addEventListener("click", () => {
+        points += (task.difficulty === "易" ? 1 : task.difficulty === "普" ? 2 : 3);
+        updateCharacter();
+        saveTasks();
+        donebtn.remove();
+        li.style.textDecoration = "line-through";
+    });
 
     const delbtn = document.createElement("button");
     delbtn.textContent = "削除";
@@ -47,6 +65,24 @@ function addTaskToList(task) {
     });
 
     li.appendChild(delbtn);
+    li.appendChild(donebtn);
     list.appendChild(li);
 }
 
+function updateCharacter() {
+    if (points < 20) {
+        charImg.src = "1.gif";
+    } else if (points < 50) {
+        charImg.src = "2.gif";
+    } else if (points < 100) {
+        charImg.src = "3.gif";
+    } else {
+        charImg.src = "4.gif";
+    }
+}
+
+resetBtn.addEventListener("click", () => {
+    points = 0;
+    saveTasks();
+    updateCharacter();
+});
